c3be96ba818dbe4b2a0f8cbcae6b1bd8430f49c0
Vu Phuc Van <vuphucvantv@gmail.com>
2023-11-17 21:56:50 +0700

    new update

---
+++ BTLW/Controllers/MainController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTLW.Controllers
+{
+	public class MainController : Controller
+	{
+		public IActionResult Home()
+		{
+			return View();
+		}
+
+		public IActionResult Shop()
+		{
+			return View();
+		}
+
+		public IActionResult Detail()
+		{
+			return View();
+		}
+
+		public IActionResult Home2()
+		{
+			return View();
+		}
+
+		public IActionResult Blog()
+		{
+			return View();
+		}
+
+		public IActionResult ContactUs()
+		{
+			return View();
+		}
+
+		public IActionResult AboutUs()
+		{
+			return View();
+		}
+	}
+}
