f5e61d761ae72536cc828c0f62f53b85a5d169cb
doduckhanh <114272797+doduckhanh@users.noreply.github.com>
2023-11-18 21:44:07 +0700

    Ajax-1

---
+++ BTLW/Models/ProductModel/Product.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BTLW.Models.ProductModel
+{
+    public class Product
+    {
+        public string MaNoiThat { get; set; } = null!;
+
+        public string? TenNoiThat { get; set; }
+
+        public string Maloai { get; set; } = null!;
+
+        public int? SoLuong { get; set; }
+
+        public int? DonGiaNhap { get; set; }
+
+        public int? DonGiaBan { get; set; }
+
+        public string? Anh { get; set; }
+    }
+}
