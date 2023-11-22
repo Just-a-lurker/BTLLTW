8c9f46435d5065ca6ec60930200e0f706a761212
Djinhljnh <linhdinh210703@gmail.com>
2023-11-22 17:33:16 +0700

    updateBTLTK

--- BTLW/Models/TaiKhoan.cs
+++ BTLW/Models/TaiKhoan.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BTLW.Models;
 
 public partial class TaiKhoan
 {
     public int MaTk { get; set; }
-    [Required(ErrorMessage = "TenTK cannot be blank")]
-    [StringLength(50, MinimumLength = 3)]
+
     public string? TenTk { get; set; }
-    [Required(ErrorMessage = "MatKhau cannot be blank")]
-    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$")]
+
     public string? MatKhau { get; set; }
 
     public bool? LoaiTk { get; set; }
+
 }
