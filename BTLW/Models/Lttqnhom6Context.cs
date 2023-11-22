fd4aa63ce65ec14a05b7893f57a7801866e7dc79
Vu Phuc Van <vuphucvantv@gmail.com>
2023-11-22 21:00:29 +0700

    finish

--- BTLW/Models/Lttqnhom6Context.cs
+++ BTLW/Models/Lttqnhom6Context.cs
@@ -50,7 +50,7 @@ public partial class Lttqnhom6Context : DbContext
     public virtual DbSet<TheLoai> TheLoais { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Data Source=DLINH\\SQLEXPRESS;Initial Catalog=Lttqnhom6;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        => optionsBuilder.UseSqlServer("Data Source=MSI\\SQLEXPRESS;Initial Catalog=Lttqnhom6;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
